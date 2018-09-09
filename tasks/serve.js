009be99da55a38b01e0fe66bd31ca2218e4eba86
Aquariuslt <superaquariuslt@gmail.com>
2018-09-09 22:55:36 +0800

    [Webpack] Update webpack-dev-server entry hacks after webpack-dev-server release 3.1.7.

--- tasks/serve.js
+++ tasks/serve.js
@@ -4,13 +4,13 @@ import log from 'fancy-log';
 
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
-import addDevServerEntrypoints from 'webpack-dev-server/lib/util/addDevServerEntrypoints';
+import addEntries from 'webpack-dev-server/lib/utils/addEntries';
 
 import webpackDevConfig from '../config/webpack.dev.babel';
 
 gulp.task('serve', function() {
   log.info('Webpack building.');
-  addDevServerEntrypoints(webpackDevConfig, webpackDevConfig.devServer);
+  addEntries(webpackDevConfig, webpackDevConfig.devServer);
   let compilerConfig = webpack(webpackDevConfig);
   new WebpackDevServer(compilerConfig, webpackDevConfig.devServer).listen(webpackDevConfig.devServer.port, webpackDevConfig.devServer.host, function(error) {
     if (error) {
