17fa3a2b5fdf367b73cfbc217e265447fa79032e
Aquariuslt <superaquariuslt@gmail.com>
2018-10-04 14:26:24 +0800

    [UT] Add unittest for App.vue

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -1,10 +1,21 @@
 import localVue from '../shared/mocks/localVue';
 import Vuex from 'vuex';
-import { shallow } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import App from '@/App';
 
 describe('App.vue', () => {
 
+  let mockStore = new Vuex.Store({});
+  let wrapper = shallowMount(App, {
+    localVue,
+    store: mockStore,
+    stubs: [
+      'router-link',
+      'router-view'
+    ]
+  });
+
   it('# should mount App.vue correctly', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
   });
 });
